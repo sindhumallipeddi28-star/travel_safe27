5b78690914f14898c1c0bdb1827361d9f07f2a9f
sindhumallipeddi28-star <sindhumallipeddi28@gmail.com>
2025-09-26 15:04:17 +0530

    refactor: Remove React and UI components
    Removes React dependencies, the Vite React plugin, and all related UI components and configurations. This paves the way for a new rendering strategy or a complete rewrite.

--- components/icons/RouteIcon.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-export const RouteIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM16.5 12V3m0 9V3m0 9h-1.5m1.5 0V3m-5.25 9v5.25m0-5.25h-1.5m1.5 0h-1.5m-5.25 0v5.25m0-5.25h-1.5m1.5 0h-1.5" />
-  </svg>
-);
